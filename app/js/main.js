e90e5bc7ea250e327a5f6b8f1ad4cca951564918
grekova.d <gd@iteesweb.ru>
2023-08-25 10:59:32 +0300

    feat: add lazyLoading

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,5 @@
+import { lazyLoading } from './scripts/lazyLoading';
 import { animation } from './scripts/animation';
+
+lazyLoading();
 animation();
---
+++ app/js/scripts/lazyLoading.js
@@ -0,0 +1,9 @@
+
+export function lazyLoading() {
+    [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+        img.setAttribute('src', img.getAttribute('data-src'));
+        img.onload = function () {
+            img.removeAttribute('data-src');
+        };
+    });
+}
